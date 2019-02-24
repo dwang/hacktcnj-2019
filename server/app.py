f3d2ad3024b21add37c7d44d76168d5afc6d7acd
Daniel <hello@danielwang.dev>
2019-02-23 23:31:50 -0500

    feat: create server Flask app

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import database
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello, world"
+
+if __name__ == "__main__":
+    app.run(debug=True)
