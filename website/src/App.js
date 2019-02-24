7a83699be0428df623958713b6a78708000573df
jeffliu604@gmail.com <jeffliu604@gmail.com>
2019-02-23 19:15:57 -0500

    Sidebar

--- website/src/App.js
+++ website/src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SideNavBar from './components/SideNavBar/SideNavBar/SideNavBar.js';
 import Forum from './components/Forum/Forum/Forum.js';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars)
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showNav: false};
+  }
+
   render() {
     return (
-      <body>
         <div className="App">
+          <div id="sidenav">
+            <SideNavBar />
+          </div>
           <Forum />
         </div>
-      </body>
     );
   }
 }
