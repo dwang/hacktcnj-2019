877af5eeb087bf26b09db2c7f19ffdfe1d41ce43
Daniel <hello@danielwang.dev>
2019-02-24 12:48:17 -0500

    feat(database):

--- server/app.py
+++ server/app.py
@@ -19,12 +19,11 @@ def getIncidents():
 def addIncident():
     message = request.form.get("message")
     current_time = str(int(time.time() * 1000.0 / 13.0))
-    image_name = current_time
+    image_name = current_time + ".jpg"
     image = request.files["image"]
-    image.save("images/" + image_name + ".jpg")
+    image.save(image_name)
     label = image_recognition.get_labels(image_name)
 
-    
     database.addPost(message, image_name, label, current_time)
     return redirect("https://reliefgrid.net")
 
