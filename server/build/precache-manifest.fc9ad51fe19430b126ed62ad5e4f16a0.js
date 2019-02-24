be6965ba0fc1b03de958febf20568a3f2f4cc50a
Daniel <hello@danielwang.dev>
2019-02-24 12:31:22 -0500

    feat: update react app

---
+++ server/build/precache-manifest.fc9ad51fe19430b126ed62ad5e4f16a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b808ec50e10dd33ff32",
+    "url": "/static/css/main.52f35ed4.chunk.css"
+  },
+  {
+    "revision": "8b808ec50e10dd33ff32",
+    "url": "/static/js/main.8b808ec5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ee9ea17fa940d6890076",
+    "url": "/static/js/2.ee9ea17f.chunk.js"
+  },
+  {
+    "revision": "dbbb80594d77856d7234c307cbac7b3d",
+    "url": "/static/media/logo.dbbb8059.svg"
+  },
+  {
+    "revision": "2ebc6bdf665f6b417972a18f66331f59",
+    "url": "/static/media/background-image.2ebc6bdf.jpg"
+  },
+  {
+    "revision": "2d600d5d8afd72dbb36f6ab95dd954da",
+    "url": "/index.html"
+  }
+];
