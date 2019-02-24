4a5b91ab90072c89998ca06b2dd1ae7db071f9a0
Daniel <hello@danielwang.dev>
2019-02-23 19:18:55 -0500

    fix: remove users from database

---
+++ server/database.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+
+import logging
+
+logging.basicConfig(filename="hacktcnj.log", level=logging.INFO)
+
+db = MongoClient("35.199.32.115", 27017)['hacktcnj']
+posts = db.posts
+
+
+def addPost(user, message, time):
+    post = {
+        "user": str(user),
+        "message": str(message),
+        "time": str(time)
+    }
+
+    logging.info(post)
+    posts.insert_one(post)
+
+
+def getPost():
+    return posts
+
+
+def clearDatabase():
+    posts.delete_many({})
+
+
+if __name__ == "__main__":
+    clearDatabase()
