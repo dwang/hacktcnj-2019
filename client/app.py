cd2ffcf89029fa784a2e7664ab0e5c49b5d626cd
Daniel <hello@danielwang.dev>
2019-02-24 06:30:18 -0500

    refactor: move database code to server

--- client/app.py
+++ client/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect
-import database
 import json
 
 app = Flask(__name__)
@@ -8,20 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def serve(name=None):
     return render_template('session.html', name=name)
-
-@app.route('/api/getIncidents')
-def getIncidents():
-    return database.getPosts()
-
-
-@app.route('/api/addIncident', methods=['POST'])
-def addIncident():
-    user = request.form.get("user")
-    message = request.form.get("message")
-    time = request.form.get("time")
-
-    database.addIncident(user, message, time)
-    return redirect("https://reliefgrid.net")
 
 
 if __name__=="__main__":
